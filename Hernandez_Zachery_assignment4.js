0d00401b6dfd1350ba4e15f4c59b81a447dfa90f
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-03-19 18:56:16 -0500

    Added in first portion of library with global variables and nested functions. Also, added in first strin manipulation function. All bugs woked out at this point.

--- Hernandez_Zachery_assignment4.js
+++ Hernandez_Zachery_assignment4.js
@@ -1,1 +1,37 @@
+/* 
+Zachery Hernandez
+February 27, 2012
+Deliverable 1
+BlackBerry, Themes, SVG
+*/
+// String Manipulation //
+var fsoLibrary = function (name) {
+	// Private //
+	var name;
+	
+	var getName = function () {
+		return name
+	};
+	
+	return {
+	// Public //
+	"name": getName,
+	};
+};
+var instructor = fsoLibrary("lyndon modomo");
+console.log("My Instructor is " + instructor.name() + ".");
+
+String.prototype.toTitleCase = function () {
+	var txt = "";
+	var wrds = this.split(" ");
+	for(key in wrds)
+	{
+	txt += " " + wrds[key].substr(0,1).toUpperCase()
+	 + wrds[key].substr(1,wrds[key].length);
+	}
+   return txt;
+}
+var string = instructor.name();
+string = string.toTitleCase();
+console.log("My Instructor is" + string.toTitleCase() + ".");
 alert("JavaScript works!");
